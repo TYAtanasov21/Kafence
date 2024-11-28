48eb736364b83eac9c8043f94957b54bdf743e99
Todor Atanasov <TYAtanasov21@codingburgas.bg>
2024-11-28 11:20:56 +0200

    Edit maps for android

---
+++ Project/src/components/shared/clusturerOptions.tsx
@@ -0,0 +1,14 @@
+const clustererOptions = {
+    gridSize: 50, 
+    maxZoom: 15, 
+    styles: [
+      {
+        url: "https://img.icons8.com/?size=100&id=VW9mAoyk46FP&format=png&color=000000",
+        height: 40,
+        width: 40,
+        textColor: "black",
+        textSize: 12,
+      },
+    ],
+  };
+  export default clustererOptions;
